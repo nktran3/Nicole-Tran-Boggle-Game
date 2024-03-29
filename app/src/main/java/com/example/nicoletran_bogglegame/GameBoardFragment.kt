bc2e943d5417a6c9d2d65354812ef519938b9c1f
Nicole Tran <nktran3@bu.edu>
2024-03-29 01:06:07 -0400

    allow the user to shake the device to start a new game

--- app/src/main/java/com/example/nicoletran_bogglegame/GameBoardFragment.kt
+++ app/src/main/java/com/example/nicoletran_bogglegame/GameBoardFragment.kt
@@ -1,10 +1,10 @@
 package com.example.nicoletran_bogglegame
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.*
 import android.widget.*
-
 
 class GameBoardFragment : Fragment() {
     private val selectedLetters = StringBuilder()
--- app/src/main/java/com/example/nicoletran_bogglegame/MainActivity.kt
+++ app/src/main/java/com/example/nicoletran_bogglegame/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.nicoletran_bogglegame
 
+import android.content.*
+import android.hardware.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import java.util.*
+import kotlin.math.*
 
 class MainActivity : AppCompatActivity(), GameCommunication {
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
         val gameBoardFragment = GameBoardFragment()
         supportFragmentManager.beginTransaction().replace(R.id.gameFragment, gameBoardFragment).commit()
+
     }
 
     override fun updateScore(score: Int) {
@@ -22,4 +40,38 @@ class MainActivity : AppCompatActivity(), GameCommunication {
         val gameBoardFragment = supportFragmentManager.findFragmentById(R.id.gameFragment) as? GameBoardFragment
         gameBoardFragment?.resetGame()
     }
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // fetch x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            // get current accelerations with the help of fetched x,y,z values
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            // acceleration value is over 10 launch google maps intent and play greeting
+            if (acceleration > 10) {
+                resetGame()
+
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
 }
